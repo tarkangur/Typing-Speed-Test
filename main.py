1a5b20ec70423d2d9671c3a6cf4dd93f284eb597
tarkangur <142388310+tarkangur@users.noreply.github.com>
2024-09-25 23:15:37 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+
+window = tk.Tk()
+window.title("Typing Speed Test App")
+window.config(padx=50, pady=50, bg="white")
+window.geometry("800x600")
+
+
+
+
+
+
+window.mainloop()
