0c29510ec8f7d53aaf439b50473d52361600e59a
tarkangur <142388310+tarkangur@users.noreply.github.com>
2024-09-26 21:47:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ window.config(padx=50, pady=50, bg="white")
 window.geometry("800x600")
 
 
+restart_button = tk.Button(window, text="Restart")
+restart_button.pack(side=tk.TOP, anchor=tk.E, pady=20)
+
+canvas = tk.Canvas(window, bg="white", width=400, height=300)
+canvas.pack()
+
+entry = tk.Entry(window, bg="#F8F8FF", width=70)
+entry.pack(pady=20)
 
 
 
