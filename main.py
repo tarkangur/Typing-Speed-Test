d9318cfbcf984c3827d48b2f76c240fa17a91d4d
tarkangur <142388310+tarkangur@users.noreply.github.com>
2024-10-02 18:28:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
 import tkinter as tk
 
 
+enter_list = []
+
+
 window = tk.Tk()
 window.title("Typing Speed Test App")
 window.config(padx=50, pady=50, bg="white")
 window.geometry("800x600")
 
 
+def countdown(count):
+    timer["text"] = count
+
+    if count > 0:
+        window.after(1000, countdown, count-1)
+
+    else:
+        popup = tk.Toplevel()
+        popup.title("Result")
+
+
 restart_button = tk.Button(window, text="Restart")
-restart_button.pack(side=tk.TOP, anchor=tk.E, pady=20)
+restart_button.grid(column=2, row=0, pady=20)
+
+timer = tk.Label(window, bg="white", font=("Courier", 15, "bold"))
+timer.grid(column=1, row=0)
 
 canvas = tk.Canvas(window, bg="white", width=400, height=300)
-canvas.pack()
+canvas.grid(column=1, row=1, columnspan=2, padx=145)
 
-entry = tk.Entry(window, bg="#F8F8FF", width=70)
-entry.pack(pady=20)
+entry = tk.Entry(window, bg="#F8F8FF", width=27, font="Helvetica 20 bold", justify="center")
+entry.grid(column=1, row=2, columnspan=2, pady=20)
 
-
+countdown(60)
 
 
 window.mainloop()
