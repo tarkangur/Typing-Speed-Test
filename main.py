b8628871a91e6d9292166faedb2b6803ad679504
tarkangur <142388310+tarkangur@users.noreply.github.com>
2024-10-21 23:13:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import tkinter as tk
 
-
+words_list = []
 enter_list = []
+correct_chars = 0
+timer_started = False
+time = 60
 
 
 window = tk.Tk()
@@ -21,7 +24,12 @@ def countdown(count):
         popup.title("Result")
 
 
-restart_button = tk.Button(window, text="Restart")
+def restart():
+    countdown(60)
+    timer["text"] = time
+
+
+restart_button = tk.Button(window, text="Restart", command=restart)
 restart_button.grid(column=2, row=0, pady=20)
 
 timer = tk.Label(window, bg="white", font=("Courier", 15, "bold"))
@@ -32,8 +40,7 @@ canvas.grid(column=1, row=1, columnspan=2, padx=145)
 
 entry = tk.Entry(window, bg="#F8F8FF", width=27, font="Helvetica 20 bold", justify="center")
 entry.grid(column=1, row=2, columnspan=2, pady=20)
-
-countdown(60)
+entry.bind("<KeyPress>", countdown(60))
 
 
 window.mainloop()
